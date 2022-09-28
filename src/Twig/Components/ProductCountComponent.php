3cb35cb183f30cbaac5023b0f2563fdf0145046a
Mrtn Schndlr <mrtnschndlr@gmail.com>
2022-09-28 12:22:03 +0200

    Create ProductCountComponent.php

---
+++ src/Twig/Components/ProductCountComponent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MyPlugin\Twig\Components;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Aggregation\Metric\CountAggregation;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\AggregationResult\Metric\CountResult;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+
+class ProductCountComponent
+{
+
+    public SalesChannelContext $context;
+
+    public function __construct(private EntityRepositoryInterface $productRepository)
+    {
+    }
+
+    public function getProductCount(): int
+    {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('product.active', true));
+        $criteria->addAggregation(new CountAggregation('productCount', 'product.id'));
+
+        /** @var CountResult $productCountResult */
+        $productCountResult = $this->productRepository
+            ->aggregate($criteria, $this->context->getContext())
+            ->get('productCount');
+
+        return $productCountResult->getCount();
+    }
+}
